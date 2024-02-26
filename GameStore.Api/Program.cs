df903a358a6922f142f7fea598adc6359744ecde
Giuseppe Cascone <casgiuseppe.88@gmail.com>
2024-02-26 18:55:17 +0100

    impostazione iniziale

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,8 @@
+//Qui viene creato un oggetto WebApplicationBuilder. Questo oggetto viene utilizzato per configurare e costruire l'applicazione web ASP.NET Core.
+var builder = WebApplication.CreateBuilder(args);
+//L'istruzione successiva chiama il metodo Build sull'oggetto WebApplicationBuilder, restituendo un oggetto WebApplication. Questo oggetto rappresenta l'applicazione web ASP.NET Core in sé.
+var app = builder.Build();
+
+app.MapGet("/", () => "Karim Muffa!");
+
+app.Run();
